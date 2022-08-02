f0ecc669015c1eb9b90e237a698f5ef7a53ee969
HoangQuan <quanna3799@gmail.com>
2022-08-02 23:15:25 +0700

    Updates

---
+++ precache-manifest.58183de156bbfe875f29e5fb39f4e332.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7f6ee7013011013521999ad9116f6c7",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "af63707d0dbe13743b78",
+    "url": "/todo-react/static/css/main.2a9b38db.chunk.css"
+  },
+  {
+    "revision": "45ec1c0ef2bee29aaf39",
+    "url": "/todo-react/static/js/2.be548e55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.be548e55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af63707d0dbe13743b78",
+    "url": "/todo-react/static/js/main.56e6b85f.chunk.js"
+  },
+  {
+    "revision": "282bd5ffe04c65b8fc9d",
+    "url": "/todo-react/static/js/runtime-main.95fa7ce3.js"
+  }
+]);
